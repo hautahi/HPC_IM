beecb0c4674894df4bff364293cfd23d6122948a
hautahi <hrk55@cornell.edu>
2018-08-14 11:01:28 -0400

    Graph code and gpu results.

--- analysis/1.gather_results.py
+++ analysis/1.gather_results.py
@@ -90,3 +90,123 @@ for node in node_list:
 
 d = pd.DataFrame({'nodes':node_list,'1':desk,'4': aws4,'8': aws8,'36': aws36,'72': aws72})
 d.to_csv("./results/multicore-loop2-Exact.csv", index=False)
+
+#-------------------------
+# Construct Loop 1: GPU
+# -------------------------
+
+node_list = [10,20,30,40,50,60,70,80,90,100,500,1000,5000,10000]
+aws_p2_64, aws_p2_128, aws_p2_256 = [],[],[]
+aws_p8_64, aws_p8_128, aws_p8_256 = [],[],[]
+for node in node_list:
+
+    # The results from p2.x AWS instance
+    folder = "../GPU/output_p2_1/"
+    
+    fname = folder + "loop1_gpu_n" + str(node)+"_mc70000_p0.2_th64_aws.csv"
+    aws_p2_64.append(get_results(fname))
+    
+    fname = folder + "loop1_gpu_n" + str(node)+"_mc70000_p0.2_th128_aws.csv"
+    aws_p2_128.append(get_results(fname))
+        
+    fname = folder + "loop1_gpu_n" + str(node)+"_mc70000_p0.2_th256_aws.csv"
+    aws_p2_256.append(get_results(fname))
+        
+    # The results from p2.x8 AWS instance
+    folder = "../GPU/output_p2_8/"
+
+    fname = folder + "loop1_gpu_n" + str(node)+"_mc70000_p0.2_th64_aws.csv"
+    aws_p8_64.append(get_results(fname))
+    
+    fname = folder + "loop1_gpu_n" + str(node)+"_mc70000_p0.2_th128_aws.csv"
+    aws_p8_128.append(get_results(fname))
+        
+    fname = folder + "loop1_gpu_n" + str(node)+"_mc70000_p0.2_th256_aws.csv"
+    aws_p8_256.append(get_results(fname))
+
+    # The results from p2.x16 AWS instance
+
+d = pd.DataFrame({'nodes':node_list,
+                  '1gpu-64thread': aws_p2_64,'1gpu-128thread': aws_p2_128,'1gpu-256thread': aws_p2_256,
+                  '8gpu-64thread': aws_p8_64,'8gpu-128thread': aws_p8_128,'8gpu-256thread': aws_p8_256})
+d.to_csv("./results/gpu-loop1.csv", index=False)
+
+#-------------------------
+# Construct Loop 2-RIS: GPU
+# -------------------------
+
+node_list = [10,20,30,40,50,60,70,80,90,100,500,1000,5000,10000]
+aws_p2_64, aws_p2_128, aws_p2_256 = [],[],[]
+aws_p8_64, aws_p8_128, aws_p8_256 = [],[],[]
+for node in node_list:
+
+    # The results from p2.x AWS instance
+    folder = "../GPU/output_p2_1/"
+    
+    fname = folder + "loop2-RIS_gpu_n" + str(node)+"_k4_mc70000_p0.2_th64_aws.csv"
+    aws_p2_64.append(get_results(fname))
+    
+    fname = folder + "loop2-RIS_gpu_n" + str(node)+"_k4_mc70000_p0.2_th128_aws.csv"
+    aws_p2_128.append(get_results(fname))
+        
+    fname = folder + "loop2-RIS_gpu_n" + str(node)+"_k4_mc70000_p0.2_th256_aws.csv"
+    aws_p2_256.append(get_results(fname))
+        
+    # The results from p2.x8 AWS instance
+    folder = "../GPU/output_p2_8/"
+
+    fname = folder + "loop2-RIS_gpu_n" + str(node)+"_k4_mc70000_p0.2_th64_aws.csv"
+    aws_p8_64.append(get_results(fname))
+    
+    fname = folder + "loop2-RIS_gpu_n" + str(node)+"_k4_mc70000_p0.2_th128_aws.csv"
+    aws_p8_128.append(get_results(fname))
+        
+    fname = folder + "loop2-RIS_gpu_n" + str(node)+"_k4_mc70000_p0.2_th256_aws.csv"
+    aws_p8_256.append(get_results(fname))
+
+    # The results from p2.x16 AWS instance
+
+d = pd.DataFrame({'nodes':node_list,
+                  '1gpu-64thread': aws_p2_64,'1gpu-128thread': aws_p2_128,'1gpu-256thread': aws_p2_256,
+                  '8gpu-64thread': aws_p8_64,'8gpu-128thread': aws_p8_128,'8gpu-256thread': aws_p8_256})
+d.to_csv("./results/gpu-loop2-RIS.csv", index=False)
+
+#-------------------------
+# Construct Loop 2-Exact: GPU
+# -------------------------
+
+node_list = [10,20,30,40,50,60,70,80,90,100]
+aws_p2_64, aws_p2_128, aws_p2_256 = [],[],[]
+aws_p8_64, aws_p8_128, aws_p8_256 = [],[],[]
+for node in node_list:
+
+    # The results from p2.x AWS instance
+    folder = "../GPU/output_p2_1/"
+    
+    fname = folder + "loop2-Exact_gpu_n" + str(node)+"_k4_mc70000_p0.2_th64_aws.csv"
+    aws_p2_64.append(get_results(fname))
+    
+    fname = folder + "loop2-Exact_gpu_n" + str(node)+"_k4_mc70000_p0.2_th128_aws.csv"
+    aws_p2_128.append(get_results(fname))
+        
+    fname = folder + "loop2-Exact_gpu_n" + str(node)+"_k4_mc70000_p0.2_th256_aws.csv"
+    aws_p2_256.append(get_results(fname))
+        
+    # The results from p2.x8 AWS instance
+    folder = "../GPU/output_p2_8/"
+
+    fname = folder + "loop2-Exact_gpu_n" + str(node)+"_k4_mc70000_p0.2_th64_aws.csv"
+    aws_p8_64.append(get_results(fname))
+    
+    fname = folder + "loop2-Exact_gpu_n" + str(node)+"_k4_mc70000_p0.2_th128_aws.csv"
+    aws_p8_128.append(get_results(fname))
+        
+    fname = folder + "loop2-Exact_gpu_n" + str(node)+"_k4_mc70000_p0.2_th256_aws.csv"
+    aws_p8_256.append(get_results(fname))
+
+    # The results from p2.x16 AWS instance
+
+d = pd.DataFrame({'nodes':node_list,
+                  '1gpu-64thread': aws_p2_64,'1gpu-128thread': aws_p2_128,'1gpu-256thread': aws_p2_256,
+                  '8gpu-64thread': aws_p8_64,'8gpu-128thread': aws_p8_128,'8gpu-256thread': aws_p8_256})
+d.to_csv("./results/gpu-loop2-Exact.csv", index=False)
--- analysis/2.make_plots.py
+++ analysis/2.make_plots.py
@@ -1,5 +1,6 @@
 """
 This program creates plots for the report.
+@author: luke
 """
 
 import pandas as pd
@@ -9,16 +10,154 @@ from matplotlib import pyplot as plt
 # Multicore: Loop 1 Running Time vs Network Size by Cores
 # -------------------------
 
-d = pd.read_csv("./results/multicore-loop1.csv")
+d_L1 = pd.read_csv("./results/multicore-loop1.csv",index_col=5)
+cols = [int(x) for x in d_L1.columns]
+cols= sorted(cols)
+cols = [str(x) for x in cols]
+d_L1=d_L1[cols]
+
+names=list(d_L1.index)
+names=[str(x) for x in names]
+name_index=range(len(names))
+values=d_L1.values
+
+plt.plot(name_index, values)
+plt.xticks(name_index, names)
+plt.legend(([str(x)+' cores' for x in d_L1.columns]))
+plt.xlabel('Nodes')
+plt.ylabel('Runtime, seconds')
+plt.title('Loop 1 Running Time with Network Size & Cores')
+fig = plt.gcf()
+fig.set_size_inches(11, 5)
+plt.savefig('./graphs/L1.png')
+plt.clf()
+
+
 
 #-------------------------
 # Multicore: Loop 2-RIS Running Time vs Network Size by Cores
 # -------------------------
 
-d = pd.read_csv("./results/multicore-loop2-Exact.csv")
+d_RIS = pd.read_csv("./results/multicore-loop2-RIS.csv",index_col=5)
+cols = [int(x) for x in d_RIS.columns]
+cols= sorted(cols)
+cols = [str(x) for x in cols]
+d_RIS=d_RIS[cols]
+
+names=list(d_RIS.index)
+names=[str(x) for x in names]
+name_index=range(len(names))
+values=d_RIS.values
+
+plt.plot(name_index, values)
+plt.xticks(name_index, names)
+plt.legend(([str(x)+' cores' for x in d_RIS.columns]))
+plt.xlabel('Nodes')
+plt.ylabel('Runtime, seconds')
+plt.title('Loop 2-RIS Running Time with Network Size & Cores')
+fig = plt.gcf()
+fig.set_size_inches(11, 5)
+plt.savefig('./graphs/L2_RIS.png')
+plt.clf()
 
 #-------------------------
 # Multicore: Loop 2-Exact Running Time vs Network Size by Cores
 # -------------------------
 
-d = pd.read_csv("./results/multicore-loop2-RIS.csv")
+d_ex = pd.read_csv("./results/multicore-loop2-Exact.csv",index_col=5)
+cols = [int(x) for x in d_ex.columns]
+cols= sorted(cols)
+cols = [str(x) for x in cols]
+d_ex=d_ex[cols]
+
+names=list(d_ex.index)
+names=[str(x) for x in names]
+name_index=range(len(names))
+values=d_ex.values
+
+plt.plot(name_index, values)
+plt.xticks(name_index, names)
+plt.legend(([str(x)+' cores' for x in d_ex.columns]))
+plt.xlabel('Nodes')
+plt.ylabel('Runtime, seconds')
+plt.title('Loop 2-Exact Running Time with Network Size & Cores')
+fig = plt.gcf()
+fig.set_size_inches(11, 5)
+plt.savefig('./graphs/L2_Exact.png')
+plt.clf()
+
+#-------------------------
+# Multicore: Loop 1- Speed Improvement with Number of Cores
+# -------------------------
+names=list(d_L1.loc[100000].index)
+names=[str(x) for x in names]
+name_index=range(len(names))
+values=d_L1.loc[100000]
+
+plt.plot(name_index, values)
+plt.xticks(name_index, names)
+plt.xlabel('Cores')
+plt.ylabel('Runtime, seconds')
+plt.title('Loop 1- 100,000 nodes')
+fig = plt.gcf()
+fig.set_size_inches(11, 5)
+plt.savefig('./graphs/L1_cores_speedup.png')
+plt.clf()
+
+#-------------------------
+# Multicore: Loop 2, RIS- Speed Improvement with Number of Cores
+# -------------------------
+names=list(d_RIS.loc[100000].index)
+names=[str(x) for x in names]
+name_index=range(len(names))
+values=d_RIS.loc[100000]
+
+plt.plot(name_index, values)
+plt.xticks(name_index, names)
+plt.xlabel('Cores')
+plt.ylabel('Runtime, seconds')
+plt.title('Loop 2, RIS- 100,000 nodes')
+fig = plt.gcf()
+fig.set_size_inches(11, 5)
+plt.savefig('./graphs/L2_RIS_cores_speedup.png')
+plt.clf()
+
+#-------------------------
+# Multicore: Loop 2, Exact - Speed Improvement with Number of Cores
+# -------------------------
+names=list(d_ex.loc[30].index)
+names=[str(x) for x in names]
+name_index=range(len(names))
+values=d_ex.loc[30]
+
+plt.plot(name_index, values)
+plt.xticks(name_index, names)
+plt.xlabel('Cores')
+plt.ylabel('Runtime, seconds')
+plt.title('Loop 2, RIS- 30 nodes')
+fig = plt.gcf()
+fig.set_size_inches(11, 5)
+plt.savefig('./graphs/L2_exact_cores_speedup.png')
+plt.clf()
+
+#-------------------------
+# Multicore: Computation of Exact Solution
+# -------------------------
+d_ex = pd.read_csv("./results/multicore-loop2-Exact.csv",index_col=5)
+d_ex=pd.DataFrame(d_ex['72'])
+
+names=list(d_ex.index)
+names=[str(x) for x in names]
+name_index=range(len(names))
+values=d_ex.values
+
+plt.plot(name_index, values)
+plt.xticks(name_index, names)
+plt.legend(([str(x)+' cores' for x in d_ex.columns]))
+plt.xlabel('Nodes')
+plt.ylabel('Runtime, seconds')
+plt.title('Loop 2-Exact Running Time with Network Size using 72 Cores')
+fig = plt.gcf()
+fig.set_size_inches(11, 5)
+plt.savefig('./graphs/L2_Exact_72c.png')
+plt.clf()
